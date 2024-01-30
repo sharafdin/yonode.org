d96f0746808e9cbe613a3b1ec7a7b028de9757de
mchamoudadev <mchamuuda@gmail.com>
2024-01-31 00:12:27 +0300

    ingore liniting

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const nextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
+
     ignoreDuringBuilds: true,
   },
   webpack(config) {
