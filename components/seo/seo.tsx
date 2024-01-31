4ff52b4993da49b007c41abc1aeedafed6d27ea9
isasharafdin <isasharafdin@gmail.com>
2024-01-31 16:57:10 +0300

    configured seo

--- components/seo/seo.tsx
+++ components/seo/seo.tsx
@@ -6,8 +6,8 @@ export interface SEOProps extends NextSeoProps {}
 
 export const SEO = ({ title, description, ...props }: SEOProps) => (
   <NextSeo
-    title={title}
-    description={description}
+    title={siteConfig.seo.title}
+    description={siteConfig.seo.description}
     openGraph={{ ...siteConfig.seo.openGraph, title, description }}
     titleTemplate={siteConfig.seo.titleTemplate}
     twitter={siteConfig.seo.twitter}
