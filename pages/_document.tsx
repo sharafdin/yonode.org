1b082f64bd06b1a0e8d83afd71f6f0f245217d68
isasharafdin <isasharafdin@gmail.com>
2024-02-11 15:17:58 +0300

    changed favicon, changed some testimonials avaters, change twitter url, and changed the core components section

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,19 +10,19 @@ export default class Document extends NextDocument {
           <link
             rel="apple-touch-icon"
             sizes="76x76"
-            href="/static/favicons/apple-touch-icon.png"
+            href="apple-touch-icon.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/static/favicons/favicon-32x32.png"
+            href="https://avatars.githubusercontent.com/u/138872151?s=200&v=4"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/static/favicons/favicon-16x16.png"
+            href="https://avatars.githubusercontent.com/u/138872151?s=200&v=4"
           />
           <link rel="manifest" href="/static/favicons/manifest.json" />
         </Head>
