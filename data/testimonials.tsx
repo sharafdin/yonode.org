0aa4988982dfa76dffa15f55bec60cf693ae9045
Mr Sharafdin <isasharafdin@gmail.com>
2024-02-28 14:57:57 +0300

    removed 3 testimonials and added 1

--- data/testimonials.tsx
+++ data/testimonials.tsx
@@ -13,8 +13,8 @@ export default {
       ),
     },
     {
-      name: 'Abdinasir Mursal',
-      description: 'Fullstack Developer',
+      name: 'Mr Turaab',
+      description: 'Frontend Developer',
       avatar: 'https://avatars.githubusercontent.com/u/115280340?v=4',
       children: (
         <>
@@ -29,26 +29,6 @@ export default {
       children: (
         <>
           "Yonode has been a game-changer for our server development. It empowered us to focus on core business logic while dramatically reducing development hours. Our server infrastructure is now modern and efficient, thanks to Yonode's impact."
-        </>
-      ),
-    },
-    {
-      name: 'Omar Tood',
-      description: 'Software Developer',
-      avatar: 'https://avatars.githubusercontent.com/u/129175570?v=4',
-      children: (
-        <>
-          "Yonode has revolutionized our server development, allowing us to concentrate on core business logic while significantly cutting development hours. Thanks to Yonode, our server infrastructure is now both modern and highly efficient."
-        </>
-      ),
-    },
-    {
-      name: 'Eng Miirshe',
-      description: 'Mern Stack',
-      avatar: 'https://avatars.githubusercontent.com/u/126691024?v=4',
-      children: (
-        <>
-          "Yonode has truly transformed our server development landscape. By streamlining the process and dramatically reducing development hours, it has empowered us to prioritize core business logic. Our server infrastructure is now at the pinnacle of modernity and efficiency, all thanks to the remarkable impact of Yonode."
         </>
       ),
     },
